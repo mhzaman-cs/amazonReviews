f67b88515bc10ee5a1fee98a8d585b09931051e5
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2021-06-30 18:51:48 -0400

    Removes session token.

--- amazon/settings.py
+++ amazon/settings.py
@@ -13,7 +13,7 @@ accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/w
 accept-encoding: gzip, deflate, br
 accept-language: en-US,en;q=0.9
 cache-control: max-age=0
-cookie: session-id=132-2102806-9721268; session-id-time=2082787201l; i18n-prefs=USD; sp-cdn="L5Z9:CA"; ubid-main=130-1295885-2116668; session-token=94ZiyPtm2PSwwLY9ff3TjQ9xQ5AlNyFX+iryD6ofLlY4v1+/17fOZPNE0BKYgHgLB240kALJ8qBsBtnsul6wlvnkT5aaypvlvvP2Ei22alqAqxc8N+T4ulAkECosOPnTzmPhePmkRwtZTa1W0V/SNy6uKZSVHrf7CH6UFBo7vYRXOpzYFa3pr94YoaUgtOD7; csm-hit=tb:950J5ES2BKVQS1HBDF44+s-950J5ES2BKVQS1HBDF44|1625013306681&t:1625013306681&adb:adblk_no
+cookie: session-id=132-2102806-9721268; session-id-time=2082787201l; i18n-prefs=USD; sp-cdn="L5Z9:CA"; ubid-main=130-1295885-2116668; csm-hit=tb:950J5ES2BKVQS1HBDF44+s-950J5ES2BKVQS1HBDF44|1625013306681&t:1625013306681&adb:adblk_no
 downlink: 10
 ect: 4g
 rtt: 100
